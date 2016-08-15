3e4684940871cbfcca0c87fb8ae5fb80114fd376
raphaelbischof <raphael.bischof@gmail.com>
2016-08-15 17:47:24 +0200

    Initial Commit

---
+++ HalfModalPresenter/HalfModalPresenter.h
@@ -0,0 +1,19 @@
+//
+//  HalfModalPresenter.h
+//  HalfModalPresenter
+//
+//  Created by Raphael Bischof on 15/08/16.
+//  Copyright © 2016 InAndWin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HalfModalPresenter.
+FOUNDATION_EXPORT double HalfModalPresenterVersionNumber;
+
+//! Project version string for HalfModalPresenter.
+FOUNDATION_EXPORT const unsigned char HalfModalPresenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HalfModalPresenter/PublicHeader.h>
+
+
